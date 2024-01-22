8a9c48752e17aa2eb73133dbc6d7ae3633f73038
mkJou <joalexint@gmail.com>
2024-01-22 19:39:03 -0400

    masive updaet

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -16,23 +16,72 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
   let emails = [];
 
-  emails.push(query?.data?.email);
+  if (query?.type == 0) {
+    emails.push(query?.data?.email);
 
-  try {
-    const { data, error } = await resend.emails.send({
-      from: "Bea de Cryptohuella <notificaciones@cryptohuella.com>",
-      to: emails,
-      subject:
-        "Un nuevo certificado/credencial se ha emitido con tu correo electrónico.",
-      react: EmailTemplate({ link: query?.data?.link }),
-    });
-    console.log();
-    if (error) {
-      res.status(400).json({ error });
+    try {
+      const { data, error } = await resend.emails.send({
+        from: "Bea de Cryptohuella <notificaciones@cryptohuella.com>",
+        to: emails,
+        subject:
+          "Un nuevo certificado/credencial se ha emitido con tu correo electrónico.",
+        react: EmailTemplate({ link: query?.data?.link }),
+      });
+
+      if (error) {
+        return res.status(400).json({ error });
+      }
+
+      return res.status(200).json({ data });
+    } catch (error) {
+      return res.status(400).json({ error });
     }
+  } else {
+    Array.from(query?.data).forEach((db, index) => {
+      emails.push({
+        data: db?.data ? { ...db?.data, hash: db?.hash } : db,
+        chain: query?.chain_id,
+      });
+    });
 
-    res.status(200).json({ data });
-  } catch (error) {
-    res.status(400).json({ error });
+    let blocks = [];
+
+    const printBlocks = async () => {
+      const files = Array.from(emails);
+
+      await Promise.all(
+        files.map(async (file) => {
+          let newL = [];
+          let newLink =
+            "https://cryptohuella.com/" +
+            String(file.chain) +
+            "/" +
+            String(file.data.hash);
+          try {
+            const { data, error } = await resend.emails.send({
+              from: "Bea de Cryptohuella <notificaciones@cryptohuella.com>",
+              to: [String(file.data.EMAIL)],
+              subject:
+                "Un nuevo certificado/credencial se ha emitido con tu correo electrónico.",
+              react: EmailTemplate({ link: newLink }),
+            });
+
+            if (error) {
+              return res.status(400).json({ error });
+            }
+
+            blocks.push({ data });
+          } catch (error) {
+            return res.status(400).json({ error });
+          }
+        })
+      );
+    };
+
+    const loadDocuments = await printBlocks();
+
+    return res.status(200).json(blocks);
   }
+
+  //
 };
