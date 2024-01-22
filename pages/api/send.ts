fc47b7cb8c2fbcd842a230494d7a9ca0d18202ee
mkJou <joalexint@gmail.com>
2024-01-22 06:53:43 -0400

    ip

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -1,16 +1,23 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { EmailTemplate } from '../../components/email-template';
-import { Resend } from 'resend';
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+import type { NextApiRequest, NextApiResponse } from "next";
+import { EmailTemplate } from "../../components/email-template";
+import { Resend } from "resend";
+import NextCors from "nextjs-cors";
+const resend = new Resend("re_ZwvRCDxH_rCBD93udyRrSiivxEzZrjBGM");
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
   try {
     const { data, error } = await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
-      to: ['delivered@resend.dev'],
-      subject: 'Hello world',
-      react: EmailTemplate({ firstName: 'John' }),
+      from: "Acme <onboarding@resend.dev>",
+      to: ["joalexint@gmail.com"],
+      subject: "Hello world",
+      react: EmailTemplate({ firstName: "John" }),
     });
 
     if (error) {
@@ -21,4 +28,4 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   } catch (error) {
     res.status(400).json({ error });
   }
-}
+};
